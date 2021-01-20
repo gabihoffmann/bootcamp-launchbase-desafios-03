b789aa95822f53f412eafa34b80f4937bbd41a8a
Gabi Hoffmann <hoffmann.agf@gmail.com>
2021-01-19 22:22:52 -0300

    update images source

--- desafios/challenge/data/data.js
+++ desafios/challenge/data/data.js
@@ -1,6 +1,6 @@
 const about = {
     name : "Rocketseat",
-    logoUrl: "https://rocketseat.com.br/static/images/logo-rocketseat.svg",
+    logoUrl: "/image/rocketseat-logo.png",
     description:{
         title: "As melhores tecnologias em programação, direto ao ponto e do jeito certo.",
         text: "No meio de tanta informação e da quantidade de ferramentas que surgem todos os dias, você precisa de alguém que te leve na direção certa."
@@ -24,21 +24,21 @@ const about = {
 const courses = [
     {
         name: "starter",
-        logoUrl: "https://skylab.rocketseat.com.br/static/64c237ccff807c054339a62d53b4b402.svg",
+        logoUrl: "/image/rocketseat.png",
         description: "Torne-se um programador desejado no mercado com esses cursos gratuitos",
         modules: "5",
         price: "Free",
     },
     {
         name: "launchbase",
-        logoUrl: "https://skylab.rocketseat.com.br/static/0828532024cb46921a6b5e941f8d788d.svg",
+        logoUrl: "/image/launch.png",
         description: "Domine programação do zero e tenha acesso às melhores oportunidades do mercado",
         modules: "8",
         price: "Reserve your spot",
     },
     {
         name: "gostack",
-        logoUrl: "https://skylab.rocketseat.com.br/static/83a178a0653dab1d55e2ed7946465975.svg",
+        logoUrl: "/image/gostack.png",
         description: "Treinamento imersivo nas tecnologias mais modernas de densenvolvimento web e mobile",
         modules: "8",
         price: "Reserve your spot",
